a7c08b29369ad4fc24879b70a7f872c323ef6d08
mvpwizard <mvportiera@gmail.com>
2017-03-14 16:57:01 +0100

    initial

---
+++ tetris/Assets/Scripts/Cube.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public int target_x;
+    public int target_y;
+    public float speed;
+
+    public Group master;
+
+    void Update()
+    {
+        if (master != null)
+            return;
+        if (transform.position.x > target_x)
+        {
+            transform.Translate(new Vector3(-1, 0 ,0) * speed * Time.deltaTime);
+            if (transform.position.x < target_x)
+            {
+                transform.position = new Vector3(target_x, transform.position.y, transform.position.z);
+            }
+        }
+        if (transform.position.x < target_x)
+        {
+            transform.Translate(new Vector3(1, 0, 0) * speed * Time.deltaTime);
+            if (transform.position.x > target_x)
+            {
+                transform.position = new Vector3(target_x, transform.position.y, transform.position.z);
+            }
+        }
+        if (transform.position.y > target_y)
+        {
+            transform.Translate(new Vector3(0, -1, 0) * speed * Time.deltaTime);
+            if (transform.position.y < target_y)
+            {
+                transform.position = new Vector3(transform.position.x, target_y, transform.position.z);
+            }
+        }
+        if (transform.position.y < target_y)
+        {
+            transform.Translate(new Vector3(0, 1, 0) * speed * Time.deltaTime);
+            if (transform.position.y > target_y)
+            {
+                transform.position = new Vector3(transform.position.x, target_y, transform.position.z);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        Debug.Log("Hello");
+        if (master != null)
+            master.Collision();
+    }
+}
---
+++ tetris/Assets/Scripts/Group.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Group : MonoBehaviour
+{
+    public float speed;
+    public GameObject board;
+
+    void Update()
+    {
+        gameObject.transform.Translate(Vector3.down * Time.deltaTime * speed);
+    }
+
+    public void Collision()
+    {
+        Cube[] cubes = GetComponentsInChildren<Cube>();
+        for (int i = 0; i < cubes.Length; i++)
+        {
+            cubes[i].gameObject.transform.parent = board.transform;
+            cubes[i].gameObject.transform.rotation = Quaternion.identity;
+            cubes[i].target_x = Mathf.RoundToInt(cubes[i].transform.position.x);
+            cubes[i].target_y = Mathf.RoundToInt(cubes[i].transform.position.y);
+            cubes[i].speed = 10;
+            cubes[i].master = null;
+        }
+        Destroy(gameObject);
+    }
+}
